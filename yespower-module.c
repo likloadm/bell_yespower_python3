ce9e6a9125e59e203c5a47636d69b5aae8053010
Vasilii <Bobit300@gmail.com>
2021-12-15 18:19:59 +0300

    Update yespower-module.c

--- yespower-module.c
+++ yespower-module.c
@@ -4,27 +4,15 @@ static PyObject *yespower_getpowhash(PyObject *self, PyObject *args)
 {
     char *output;
     PyObject *value;
-#if PY_MAJOR_VERSION >= 3
     PyBytesObject *input;
-#else
-    PyStringObject *input;
-#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
     Py_INCREF(input);
     output = PyMem_Malloc(32);
-
-#if PY_MAJOR_VERSION >= 3
     yespower_hash((char *)PyBytes_AsString((PyObject*) input), output);
-#else
-    yespower_hash((char *)PyString_AsString((PyObject*) input), output);
-#endif
+
     Py_DECREF(input);
-#if PY_MAJOR_VERSION >= 3
     value = Py_BuildValue("y#", output, 32);
-#else
-    value = Py_BuildValue("s#", output, 32);
-#endif
     PyMem_Free(output);
     return value;
 }
@@ -34,7 +22,6 @@ static PyMethodDef YespowerMethods[] = {
     { NULL, NULL, 0, NULL }
 };
 
-#if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef YespowerModule = {
     PyModuleDef_HEAD_INIT,
     "yespower_hash",
@@ -47,9 +34,3 @@ PyMODINIT_FUNC PyInit_tide_yespower(void) {
     return PyModule_Create(&YespowerModule);
 }
 
-#else
-
-PyMODINIT_FUNC inittide_yespower(void) {
-    (void) Py_InitModule("tide_yespower", YespowerMethods);
-}
-#endif
